4e1736a07e267a3285ccdbebd3dd235a3f8bb117
monir-tuhin <mtuhinsoft@gmail.com>
2022-11-09 08:59:28 +0600

    life cycle hook commit first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'life-cycle-hook';
+  title = 'Angular Life Cycle Hook';
 }
